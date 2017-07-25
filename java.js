e3fb89a9936f7cb3730abdb00a3b732ec36bc6fd
Jabari Austin <austinjabari@gmail.com>
2017-07-25 17:57:09 +0000

    Initial commit of team website

---
+++ java.js
@@ -0,0 +1,6 @@
+var navToggle = document.getElementById("nav-toggle");
+
+var navItems = document.getElementById("nav-items");
+
+navToggle.addEventListener('click', function(){navItems.classList.toggle("mobile-nav-hidden");});
+
